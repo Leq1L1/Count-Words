143c04201a98570b226fd593a2c39e550098a502
Leq1L1 <26leli001@mdusd.net>
2022-11-30 15:52:24 -0800

    11/30/2022 3:52PM Unfinished

--- app/src/main/java/org/leqili/countwords/App.java
+++ app/src/main/java/org/leqili/countwords/App.java
@@ -8,6 +8,23 @@ public class App {
         return "Hello World!";
     }
 
+
+
+public class countWords {
+    public int wordCounter(String words) {
+        int wordCount = 0;
+        char[] wordChars = words.toCharArray();
+        boolean lastCharWasLetter = false;
+        for(int i = 0; i <= words.length(); i++){
+            if (wordChars[i] == ' '){
+
+            }
+        }
+        
+        return wordCount;
+    }
+}
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
